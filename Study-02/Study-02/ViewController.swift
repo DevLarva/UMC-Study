8c008eb546a64e8d8809a8c7aaaa3d15b374a8d2
백대홍 <bdh3620@naver.com>
2023-04-04 01:12:31 +0900

    2주차 추가

---
+++ Study-02/Study-02/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  Study-02
+//
+//  Created by 백대홍 on 2023/04/03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
