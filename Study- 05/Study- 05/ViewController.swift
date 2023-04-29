44477b518f22e5be165b934d1bbc293b3976549e
백대홍 <bdh3620@naver.com>
2023-04-29 18:25:10 +0900

    5주차 업데이트

--- Study- 05/Study- 05/ViewController.swift
+++ Study- 05/Study- 05/ViewController.swift
@@ -11,9 +11,16 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let button = UIButton(type: .system)
+        button.setTitle("눌러주세요", for: .normal)
+        button.addTarget(self, action: #selector(buttonTapped(_:)), for: .touchUpInside)
+        view.addSubview(button)
+        
+    
     }
 
-
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        // Handle button tap event
+    }
 }
 
