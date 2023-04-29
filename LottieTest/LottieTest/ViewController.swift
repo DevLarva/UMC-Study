1771222b1d33d224bfead031010c3232b4eeaaf4
백대홍 <bdh3620@naver.com>
2023-04-30 01:51:59 +0900

    Lottie 정상 업데이트

---
+++ LottieTest/LottieTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LottieTest
+//
+//  Created by 백대홍 on 2023/04/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
