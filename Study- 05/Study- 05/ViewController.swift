274abd85181e2a236905307e4a333a72e1dacb71
백대홍 <bdh3620@naver.com>
2023-04-29 17:55:42 +0900

    5주차 생성

---
+++ Study- 05/Study- 05/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Study- 05
+//
+//  Created by 백대홍 on 2023/04/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
