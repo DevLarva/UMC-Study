cc15a7dfc1380825ae4dfc355a55712f527bbc14
백대홍 <bdh3620@naver.com>
2023-05-19 01:02:40 +0900

    6주차 로그인 기능 완료

--- Study-06/Study-06/ViewController.swift
+++ Study-06/Study-06/ViewController.swift
@@ -8,12 +8,26 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var nametextField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    @IBOutlet weak var emailtextField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        retriveDetails()
     }
 
-
+    @IBAction func saveBtn(_ sender: Any) {
+        UserDefaults.standard.set(nametextField.text, forKey: "Name")
+        UserDefaults.standard.set(emailtextField.text, forKey: "Email")
+        UserDefaults.standard.set(passwordField.text, forKey: "Password")
+    }
+    
+    func retriveDetails() {
+        let name = UserDefaults.standard.string(forKey: "Name")
+        let email = UserDefaults.standard.string(forKey: "Email")
+        let password = UserDefaults.standard.string(forKey: "Password")
+        
+        print("당신의 이름은 \(name!), 당신의 이메일은 \(email!), 당신의 비밀번호는 \(password!)")
+    }
 }
 
