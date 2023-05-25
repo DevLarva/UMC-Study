eb3443c601e63943f74519e3243015b96bf388ac
백대홍 <bdh3620@naver.com>
2023-05-25 21:13:48 +0900

    7주차 업데이트 및 애니메이션 실습

---
+++ Study-07/Study-07/ViewController.swift
@@ -0,0 +1,52 @@
+//
+//  ViewController.swift
+//  Study-07
+//
+//  Created by 백대홍 on 2023/05/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    let myView = UIImageView()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        myView.image = UIImage(named: "heart")
+        myView.center = view.center
+        view.addSubview(myView)
+        
+        let button = UIButton(frame: CGRect(x:  (view.frame.size.width-200)/2, y: view.frame.size.height - 220, width: 200, height: 70))
+        button.backgroundColor = .black
+        button.setTitleColor(.white, for: .normal)
+        button.setTitle("시작", for: .normal)
+        button.addTarget(self, action: #selector(animate), for: .touchUpInside)
+        view.addSubview(button)
+    }
+    @objc func animate() {
+        UIView.animate(withDuration: 0.4, animations: {
+            self.myView.frame = CGRect(x: 0, y: 0, width: 400, height: 400)
+            self.myView.center = self.view.center
+        }, completion: { done in
+            if done {
+                self.shrink()
+            }
+        })
+    }
+
+    func shrink()   {
+        UIView.animate(withDuration: 1, animations: {
+            self.myView.frame = CGRect(x: 0, y: 0, width: 200, height: 200)
+            self.myView.center = self.view.center
+        },completion: { done in
+            self.animate()
+        })
+    }
+
+}
+
